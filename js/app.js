04344dc7b9f41fbdb54cb0923b9cf405b72e3742
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-01-23 17:39:56 -0700

    js changes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
 	for (var num = 1; num <= 100; num++) {
-	if (num % 3 == 0) {
+	if (num % 3 == 0 && num % 5 == 0) {
+		$('.fizzylist').append('fizzbuzz');
+	}	
+	else if (num % 3 == 0) {
 		$('.fizzylist').append('fizz');
 	}
 	else if (num % 5 == 0) {
 		$('.fizzylist').append('buzz');
 	}
-	else if (num % 3&&num % 5) {
-		$('.fizzylist').append('fizzbuzz');
-	}
 	else {
 		$('.fizzylist').append(num);
 	}
-	$('.fizzylist').append(num)
 }	
 
 }); //$(document).ready(function) end line
