81a3f47ce3e4d86f8020094e339f916d48dea238
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-01-23 18:23:00 -0700

    made final js changes and css to get rid of bullet points

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 	for (var num = 1; num <= 100; num++) {
 		if (num % 3 == 0 && num % 5 == 0) {
-			$('#fizzylist ul').append("<li> 'FIZZBUZZ' </li>");
+			$('#fizzylist ul').append("<li> FIZZBUZZ </li>");
 		}	
 		else if (num % 3 == 0) {
-			$('#fizzylist ul').append("<li> 'fizz' </li>");
+			$('#fizzylist ul').append("<li> fizz </li>");
 		}
 		else if (num % 5 == 0) {
-			$('#fizzylist ul').append("<li> 'buzz' </li>");
+			$('#fizzylist ul').append("<li> buzz </li>");
 		}
 		else {
-			$('#fizzylist ul').append("<li> + num + </li>");
+			$('#fizzylist ul').append("<li>" + num + "</li>");
 		}
 	}	
 
