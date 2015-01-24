00dede0d8eb881fcb954005ad7d75eb65680ac1a
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-01-23 17:47:26 -0700

    js changes again

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 	for (var num = 1; num <= 100; num++) {
-	if (num % 3 == 0 && num % 5 == 0) {
-		$('.fizzylist').append('fizzbuzz');
+		if (num % 3 == 0 && num % 5 == 0) {
+			$('.fizzylist').append('fizzbuzz');
+		}	
+		else if (num % 3 == 0) {
+			$('.fizzylist').append('fizz');
+		}
+		else if (num % 5 == 0) {
+			$('.fizzylist').append('buzz');
+		}
+		else {
+			$('.fizzylist').append(num);
+		}
 	}	
-	else if (num % 3 == 0) {
-		$('.fizzylist').append('fizz');
-	}
-	else if (num % 5 == 0) {
-		$('.fizzylist').append('buzz');
-	}
-	else {
-		$('.fizzylist').append(num);
-	}
-}	
 
 }); //$(document).ready(function) end line
